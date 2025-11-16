185290183bdc63ffd08c87507b9b5ff4794748b6
Kieran Werner <Kieranwerner05@gmail.com>
2025-11-16 18:57:54 +0100

    Deploy GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mindmapNEU/',   // GANZ WICHTIG
+  // GANZ WICHTIG: Repo-Name hier eintragen
+  base: '/mindmapNEU/',
 })
